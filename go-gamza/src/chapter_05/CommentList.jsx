c941c14d25e0b31da17a561d663bf74dbb61f324
zhy2on <dhwlgus5940@gmail.com>
2022-07-14 03:53:11 +0900

    Feat: comment list 객체 분리

--- go-gamza/src/chapter_05/CommentList.jsx
+++ go-gamza/src/chapter_05/CommentList.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import Comment from "./Comment"
 
+const comments = [
+	{
+		name: "오지현",
+		comment: "안녕하세요, 오지현입니다.",
+	},
+	{
+		name: "이민형",
+		comment: "I'm a child",
+	},
+	{
+		name: "이장원",
+		comment: "캔 위 메이컵 인 더 모닝",
+	},
+	{
+		name: "익스",
+		comment: "잘 부탁드립니다",
+	},
+];
+
 function CommentList(props) {
 	return (
 		<div>
-			<Comment name={"오지현"} comment={"제가 만든 첫 컴포넌트입니다."}/>
-			<Comment name={"감자"} comment={"감자에 싹이나서 이파리에 싸이싸이싸이"}/>
+			{comments.map((comment) => {
+				return (
+					<Comment name={comment.name} comment={comment.comment} />
+				);
+			})}
 		</div>
 	);
 }
