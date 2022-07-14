883eaa4d97eae12efeab446346fd9d5711f81d57
zhy2on <dhwlgus5940@gmail.com>
2022-07-14 19:28:14 +0900

    Feat: 이벤트 핸들러 연습

--- go-gamza/src/chapter_08/ConfirmButton.jsx
+++ go-gamza/src/chapter_08/ConfirmButton.jsx
@@ -1,31 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-class ConfirmButton extends React.Component {
-	constructor(props) {
-		super(props);
+// class component 이벤트 핸들러 bind 이용
+// class ConfirmButton extends React.Component {
+// 	constructor(props) {
+// 		super(props);
 
-		this.state = {
-			isConfirmed: false,
-		}
-		this.handleConfirm = this.handleConfirm.bind(this);
+// 		this.state = {
+// 			isConfirmed: false,
+// 		}
+// 		this.handleConfirm = this.handleConfirm.bind(this);
+// 	};
+
+// 	handleConfirm() {
+// 		this.setState((prevState) => ({
+// 			isConfirmed: !prevState.isConfirmed,
+// 		}));
+// 	}
+
+// 	render() {
+// 		return (
+// 			<button
+// 				onClick={this.handleConfirm}
+// 				disabled={this.state.isConfirmed}
+// 			>
+// 				{this.state.isConfirmed ? "확인됨" : "확인하기"}
+// 			</button>
+// 		);
+// 	}
+// }
+
+// // class fields syntax 사용
+// class ConfirmButton extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			isConfirmed: false,
+// 		};
+// 	}
+
+// 	handleConfirm = () => {
+// 		this.setState((prevState) => ({
+// 			isConfirmed: !prevState.isConfirmed,
+// 		}));
+// 	}
+
+// 	render() {
+// 		return (
+// 			<button
+// 				onClick={this.handleConfirm}
+// 				disabled={this.state.isConfirmed}
+// 			>
+// 				{this.state.isConfirmed ? "확인됨" : "확인하기"}
+// 			</button>
+// 		);
+// 	}
+// }
+
+// 함수 컴포넌트 이벤틑 핸들러
+function ConfirmButton(props) {
+	const [isConfirmed, setIsConfirmed] = useState(false);
+
+	const handleConfirm = () => {
+		setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
 	};
 
-	handleConfirm() {
-		this.setState((prevState) => ({
-			isConfirmed: !prevState.isConfirmed,
-		}));
-	}
-
-	render() {
-		return (
-			<button
-				onClick={this.handleConfirm}
-				disabled={this.state.isConfirmed}
-			>
-				{this.state.isConfirmed ? "확인됨" : "확인하기"}
-			</button>
-		);
-	}
+	return (
+		<button onClick={handleConfirm} disable={isConfirmed}>
+			{isConfirmed ? "확인됨" : "확인하기"}
+		</button>
+	);
 }
 
 export default ConfirmButton;
