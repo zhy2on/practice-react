ea3c48b314580623b9d472babc7c22423754b805
zhy2on <dhwlgus5940@gmail.com>
2022-07-24 15:48:15 +0900

    Feat: Composition 연습

---
+++ go-gamza/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+	return (
+		<Card title="Jihyeon Oh" backgroundColor="#4ea04e">
+			<p>안녕하세요 지현입니다.</p>
+			<p>지금 리액트 공부중입니다.</p>
+		</Card>
+	);
+}
+
+export default ProfileCard;
